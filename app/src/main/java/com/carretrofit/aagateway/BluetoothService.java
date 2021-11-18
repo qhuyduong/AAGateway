efecc2cf5179de84f65e68c63dd6d5cc6d081714
Huy Duong <huy.duong@employmenthero.com>
2021-11-19 02:02:30 +0700

    Refactor

--- app/src/main/java/com/carretrofit/aagateway/BluetoothService.java
+++ app/src/main/java/com/carretrofit/aagateway/BluetoothService.java
@@ -30,13 +30,9 @@ import java.util.UUID;
 public class BluetoothService extends Service {
     private static final String TAG = "AAGateWayBluetoothService";
     private static final UUID A2DP_UUID = UUID.fromString("00001112-0000-1000-8000-00805F9B34FB");
-    private static final UUID HFP_UUID = UUID.fromString("0000111e-0000-1000-8000-00805f9b34fb");
     private static final UUID MY_UUID = UUID.fromString("4de17a00-52cb-11e6-bdf4-0800200c9a66");
 
     private boolean running = false;
-    private BluetoothServerSocket serverSocket = null;
-    private DataInputStream inputDataStream;
-    private OutputStream outputStream;
 
     private BroadcastReceiver bluetoothReceiver =
             new BroadcastReceiver() {
@@ -84,100 +80,132 @@ public class BluetoothService extends Service {
         Log.d(TAG, "Service destroyed");
     }
 
-    private void connectToPhone(BluetoothDevice device) {
-        try {
-            device.createRfcommSocketToServiceRecord(A2DP_UUID).connect();
-            innerConnectToPhone();
-            Intent i = new Intent(this, MyService.class);
-            startService(i);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void innerConnectToPhone() {
-        try {
-            WifiInfoRequestMessage.WifiInfoRequest.Builder newBuilder =
-                    WifiInfoRequestMessage.WifiInfoRequest.newBuilder();
-            newBuilder.setIpAddress(getLocalIpAddress());
-            newBuilder.setPort(Constants.TCP_PORT);
-            sendToPhone(newBuilder.build().toByteArray(), (short) 1);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static String getLocalIpAddress() {
-        try {
-            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
-                    en.hasMoreElements(); ) {
-                NetworkInterface intf = en.nextElement();
-                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses();
-                        enumIpAddr.hasMoreElements(); ) {
-                    InetAddress inetAddress = enumIpAddr.nextElement();
-                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
-                        return inetAddress.getHostAddress();
-                    }
-                }
-            }
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private void sendToPhone(byte[] bArr, short s) throws IOException {
-        ByteBuffer allocate = ByteBuffer.allocate(bArr.length + 4);
-        allocate.put((byte) ((bArr.length >> 8) & 255));
-        allocate.put((byte) (bArr.length & 255));
-        allocate.putShort(s);
-        allocate.put(bArr);
-        outputStream.write(allocate.array());
-        readFromPhone();
-    }
-
-    private void readFromPhone() throws IOException {
-        byte[] bArr = new byte[1024];
-        inputDataStream.read(bArr);
-        short s = (short) (((bArr[2] & 255) << 8) | (bArr[3] & 255));
-        if (s == 2) {
-            WifiSecurityResponseMessage.WifiSecurityReponse.Builder newBuilder =
-                    WifiSecurityResponseMessage.WifiSecurityReponse.newBuilder();
-            newBuilder.setSsid(BluetoothAdapter.getDefaultAdapter().getName());
-            WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-            newBuilder.setBssid(wifiManager.getConnectionInfo().getMacAddress());
-            newBuilder.setAccessPointType(
-                    WifiSecurityResponseMessage.WifiSecurityReponse.AccessPointType.STATIC);
-            newBuilder.setKey(Constants.WIFI_PASSWORD);
-            newBuilder.setSecurityMode(
-                    WifiSecurityResponseMessage.WifiSecurityReponse.SecurityMode.WPA2_PERSONAL);
-            sendToPhone(newBuilder.build().toByteArray(), (short) 3);
-        }
-    }
-
     private class AAListenerThread implements Runnable {
         private BluetoothDevice device;
+        private DataInputStream inputStream;
+        private OutputStream outputStream;
 
         public AAListenerThread(BluetoothDevice dev) {
             device = dev;
         }
 
         public void run() {
+            BluetoothServerSocket serverSocket = null;
+            BluetoothSocket socket = null;
             try {
-                BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-                BluetoothServerSocket serverSocket =
-                        adapter.listenUsingRfcommWithServiceRecord(TAG, MY_UUID);
-                BluetoothSocket socket = serverSocket.accept(2000);
+                serverSocket =
+                        BluetoothAdapter.getDefaultAdapter()
+                                .listenUsingRfcommWithServiceRecord(TAG, MY_UUID);
+                socket = serverSocket.accept(10000);
                 serverSocket.close();
-                if (socket == null) {
-                    return;
-                }
 
-                inputDataStream = new DataInputStream(socket.getInputStream());
+                inputStream = new DataInputStream(socket.getInputStream());
                 outputStream = socket.getOutputStream();
+                Log.d(TAG, "Connecting to device " + device);
                 connectToPhone(device);
             } catch (Exception e) {
+                Log.e(TAG, "AAListener - error " + e.getMessage());
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        Log.e(TAG, "AAListener - error " + e.getMessage());
+                    }
+                }
+                if (outputStream != null) {
+                    try {
+                        outputStream.close();
+                    } catch (IOException e) {
+                        Log.e(TAG, "AAListener - error " + e.getMessage());
+                    }
+                }
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        Log.e(TAG, "AAListener - error " + e.getMessage());
+                    }
+                }
+                if (serverSocket != null) {
+                    try {
+                        serverSocket.close();
+                    } catch (IOException e) {
+                        Log.e(TAG, "AAListener - error " + e.getMessage());
+                    }
+                }
+            }
+        }
+
+        private void connectToPhone(BluetoothDevice device) {
+            try {
+                device.createRfcommSocketToServiceRecord(A2DP_UUID).connect();
+                innerConnectToPhone();
+                Intent i = new Intent(getApplicationContext(), MyService.class);
+                startService(i);
+            } catch (IOException e) {
                 e.printStackTrace();
+            }
+        }
+
+        private void innerConnectToPhone() {
+            try {
+                WifiInfoRequestMessage.WifiInfoRequest.Builder newBuilder =
+                        WifiInfoRequestMessage.WifiInfoRequest.newBuilder();
+                newBuilder.setIpAddress(getLocalIpAddress());
+                newBuilder.setPort(Constants.TCP_PORT);
+                sendToPhone(newBuilder.build().toByteArray(), (short) 1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private String getLocalIpAddress() {
+            try {
+                for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+                        en.hasMoreElements(); ) {
+                    NetworkInterface intf = en.nextElement();
+                    for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses();
+                            enumIpAddr.hasMoreElements(); ) {
+                        InetAddress inetAddress = enumIpAddr.nextElement();
+                        if (!inetAddress.isLoopbackAddress()
+                                && inetAddress instanceof Inet4Address) {
+                            return inetAddress.getHostAddress();
+                        }
+                    }
+                }
+            } catch (SocketException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        private void sendToPhone(byte[] bArr, short s) throws IOException {
+            ByteBuffer allocate = ByteBuffer.allocate(bArr.length + 4);
+            allocate.put((byte) ((bArr.length >> 8) & 255));
+            allocate.put((byte) (bArr.length & 255));
+            allocate.putShort(s);
+            allocate.put(bArr);
+            outputStream.write(allocate.array());
+            readFromPhone();
+        }
+
+        private void readFromPhone() throws IOException {
+            byte[] bArr = new byte[1024];
+            inputStream.read(bArr);
+            short s = (short) (((bArr[2] & 255) << 8) | (bArr[3] & 255));
+            if (s == 2) {
+                WifiSecurityResponseMessage.WifiSecurityReponse.Builder newBuilder =
+                        WifiSecurityResponseMessage.WifiSecurityReponse.newBuilder();
+                newBuilder.setSsid(BluetoothAdapter.getDefaultAdapter().getName());
+                WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                newBuilder.setBssid(wifiManager.getConnectionInfo().getMacAddress());
+                newBuilder.setAccessPointType(
+                        WifiSecurityResponseMessage.WifiSecurityReponse.AccessPointType.STATIC);
+                newBuilder.setKey(Constants.WIFI_PASSWORD);
+                newBuilder.setSecurityMode(
+                        WifiSecurityResponseMessage.WifiSecurityReponse.SecurityMode.WPA2_PERSONAL);
+                sendToPhone(newBuilder.build().toByteArray(), (short) 3);
             }
         }
     }
