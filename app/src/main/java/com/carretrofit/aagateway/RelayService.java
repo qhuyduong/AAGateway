76bc842b7638d79e678ba4217f60192930d0fa38
Huy Duong <huy.duong@employmenthero.com>
2021-11-21 13:04:42 +0700

    Remove some code

--- app/src/main/java/com/carretrofit/aagateway/RelayService.java
+++ app/src/main/java/com/carretrofit/aagateway/RelayService.java
@@ -19,8 +19,6 @@ import java.net.Socket;
 public class RelayService extends Service {
     private static final String TAG = "AAGateWayRelayService";
     private static final String USB_ACCESSORY = "/dev/usb_accessory";
-    private static final byte[] VERSION_REQUEST = {0, 3, 0, 6, 0, 1, 0, 1, 0, 2};
-    private static final byte[] VERSION_RESPONSE = {0, 3, 0, 8, 0, 2, 0, 1, 0, 4, 0, 0};
     private static final int MAX_BUFFER_LENGTH = 16384;
 
     private FileOutputStream usbOutputStream;
@@ -102,18 +100,8 @@ public class RelayService extends Service {
 
                     tcpOutputStream = socket.getOutputStream();
                     tcpInputStream = new DataInputStream(socket.getInputStream());
-                    tcpOutputStream.write(VERSION_REQUEST);
-                    tcpOutputStream.flush();
+
                     byte[] buf = new byte[MAX_BUFFER_LENGTH];
-                    tcpInputStream.read(buf);
-                    tcpCompleted = true;
-
-                    // wait for usb initialization
-                    while (!usbCompleted && running) {
-                        Log.d(TAG, "tcp - waiting for usb");
-                        Thread.sleep(100);
-                    }
-
                     int messageLength;
                     int headerLength;
 
@@ -164,22 +152,15 @@ public class RelayService extends Service {
             ParcelFileDescriptor usbFileDescriptor = null;
             try {
                 Log.d(TAG, "usb - start");
-                byte buf[] = new byte[MAX_BUFFER_LENGTH];
                 File usbFile = new File(USB_ACCESSORY);
                 usbFileDescriptor =
                         ParcelFileDescriptor.open(usbFile, ParcelFileDescriptor.MODE_READ_WRITE);
                 usbInputStream = new FileInputStream(usbFileDescriptor.getFileDescriptor());
                 usbOutputStream = new FileOutputStream(usbFileDescriptor.getFileDescriptor());
-                usbInputStream.read(buf);
-                usbOutputStream.write(VERSION_RESPONSE);
-                usbCompleted = true;
 
-                while (!tcpCompleted && running) {
-                    Log.d(TAG, "usb - waiting for local");
-                    Thread.sleep(100);
-                }
-
+                byte buf[] = new byte[MAX_BUFFER_LENGTH];
                 int length;
+
                 while (running) {
                     length = usbInputStream.read(buf);
                     tcpOutputStream.write(buf, 0, length);
